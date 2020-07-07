8a5c680d1e5df24e164e055dfb997d471e3be1e9
JuanPablo <juanp.escobar@udea.edu.co>
2020-07-07 17:19:30 -0500

    Actualización de la interfaz Mi Bilioteca V2

--- app/src/main/java/com/example/mibibliotecav2/LoginActivity.kt
+++ app/src/main/java/com/example/mibibliotecav2/LoginActivity.kt
@@ -2,9 +2,9 @@ package com.example.mibibliotecav2
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -17,21 +17,29 @@ class LoginActivity : AppCompatActivity() {
         val correo = datos?.getString("correo")
         val contrasena = datos?.getString("contrasena")
 
+        IB_facebook_login.setOnClickListener {
+            Toast.makeText(this, "Disponible próximamente", Toast.LENGTH_SHORT).show()
+        }
+
+        IB_gmail_login.setOnClickListener {
+            Toast.makeText(this, "Disponible próximamente", Toast.LENGTH_SHORT).show()
+        }
+
         BT_ingresar_registro.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivityForResult(intent, 1234)
         }
         BT_ingresar_login.setOnClickListener {
-            if(ET_contrasena_login.text.toString() == contrasena && ET_correo_login.text.toString() == correo){
+            if (ET_contrasena_login.text.toString() == contrasena && ET_correo_login.text.toString() == correo) {
                 val intent = Intent(this, MainActivity::class.java)
                 intent.putExtra("usuario", usuario)
                 intent.putExtra("correo", correo)
                 intent.putExtra("contrasena", contrasena)
                 startActivity(intent)
                 finish()
-            }else if(contrasena == null || correo == null){
+            } else if (contrasena == null || correo == null) {
                 Toast.makeText(this, "Por favor, Registrese", Toast.LENGTH_LONG).show()
-            }else{
+            } else {
                 Toast.makeText(this, "Contraseña o Usuarios incorrectos ", Toast.LENGTH_LONG).show()
             }
         }
